25b9aaea2dec42108cf3ac077409983dd74dc675
Ricardo Signes <rjbs@cpan.org>
2013-07-07 07:49:04 -0400

    dzilify

--- lib/Rubric/Entry/Formatter/KwikiFormatish.pm
+++ lib/Rubric/Entry/Formatter/KwikiFormatish.pm
@@ -1,13 +1,7 @@
+use strict;
+use warnings;
 package Rubric::Entry::Formatter::KwikiFormatish;
-our $VERSION = '0.551';
-
-=head1 NAME
-
-Rubric::Entry::Formatter::KwikiFormatish - format entries with AlmostKwikiText
-
-=head1 VERSION
-
- $Id: KwikiFormatish.pm 1165 2006-12-08 13:38:34Z rjbs $
+# ABSTRACT: format entries with AlmostKwikiText
 
 =head1 DESCRIPTION
 
@@ -15,16 +9,13 @@ This formatter will use KwikiFormatish to format entries into HTML.
 
 =cut
 
-use strict;
-use warnings;
-
 use Text::KwikiFormatish;
 
 =head1 METHODS
 
-=head2 C< as_html >
+=method as_html
 
-=head2 C< as_text >
+=method as_text
 
 =cut
 
@@ -39,25 +30,5 @@ sub as_text {
 
   return $arg->{text};
 }
-
-=head1 TODO
-
-=head1 AUTHOR
-
-Ricardo SIGNES, C<< <rjbs@cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-rubric@rt.cpan.org>, or
-through the web interface at L<http://rt.cpan.org>. I will be notified, and
-then you'll automatically be notified of progress on your bug as I make
-changes.
-
-=head1 COPYRIGHT
-
-Copyright 2005 Ricardo SIGNES.  This program is free software;  you can
-redistribute it and/or modify it under the same terms as Perl itself.
-
-=cut
 
 1;
